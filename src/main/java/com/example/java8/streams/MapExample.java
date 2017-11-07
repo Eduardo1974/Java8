b6e476fc204ccdb5855bba18630c0bf562235185
Eduardo Alves <eduardo.alves@fotosensores.com>
2017-11-07 17:40:45 -0200

    adicionado metodo forEach em mapa

--- src/main/java/com/example/java8/streams/MapExample.java
+++ src/main/java/com/example/java8/streams/MapExample.java
@@ -61,6 +61,23 @@ public class MapExample {
                 .mapToLong(n-> Long.parseLong(n) )
                 .filter(n-> n%2 == 0)
                 .forEach(System.out::println);
+
+        Map<String, Integer> items = new HashMap<>();
+        items.put("A", 10);
+        items.put("B", 20);
+        items.put("C", 30);
+        items.put("D", 40);
+        items.put("E", 50);
+        items.put("F", 60);
+
+        items.forEach((k,v)->System.out.println("Item : " + k + " Count : " + v));
+
+        items.forEach((k,v)->{
+            System.out.println("Item : " + k + " Count : " + v);
+            if("E".equals(k)){
+                System.out.println("Hello E");
+            }
+        });
     }
 }
 
